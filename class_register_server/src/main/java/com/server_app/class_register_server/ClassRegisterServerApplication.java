bac39fba92177b7152cda5f61194d602344155db
Tomasz Nowak <76777800+Tnovyloo@users.noreply.github.com>
2024-05-23 12:56:51 +0200

    Setting up docker-compose

---
+++ class_register_server/src/main/java/com/server_app/class_register_server/ClassRegisterServerApplication.java
@@ -0,0 +1,13 @@
+package com.server_app.class_register_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassRegisterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassRegisterServerApplication.class, args);
+	}
+
+}
---
+++ class_register_server/src/test/java/com/server_app/class_register_server/ClassRegisterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server_app.class_register_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRegisterServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
